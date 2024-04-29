ae8af75f3970aab3bf87eb8cccc7e40ab1307f93
Fitranda Ramadhana <68302339+Fitranda@users.noreply.github.com>
2024-04-29 21:39:57 +0700

    Buat-splash-screen

---
+++ resources/views/screen.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" >
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="container" style="
+  background-image: url('{{ url('img/gambar1.jpg') }}');
+  background-size: cover; /* Adjust as needed (cover, contain, etc.) */
+">
+<div>
+    <span class="centered-text">Poemventure</span>
+    <a href="{{ route('login') }}" class="btn-custom" style="transform: translate(-150%, 100%);">Login</a>
+    <a href="{{ route('register') }}" class="btn-custom" style="transform: translate(40%, 100%);">Daftar</a>
+</div>
+</body>
+</html>
