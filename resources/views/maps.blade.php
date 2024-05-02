27c9e90f779b33ef8e3979d5a88201b8fcba049e
Rein <musthafafaizhal@gmail.com>
2024-05-02 11:23:28 +0700

    map

---
+++ resources/views/maps.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Poemventure</title>
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" >
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="container" style="
+  background-image: url('{{ url('img/map.jpg') }}');
+  background-size: cover;
+  background-repeat: no-repeat;
+  min-height: 100vh;
+">
+<div>
+    <a href="" class="btn-danger" style="transform: translatey(750px) translatex(125px)">BAB I</a>
+    <a href="" class="btn-dooms" style="transform: translatey(325px) translatex(465px)">BAB II</a>
+    <a href="" class="btn-sea" style="transform: translatey(700px) translatex(815px)">BAB III</a>
+    <a href="" class="btn-dwarf" style="transform: translatey(325px) translatex(1120px)">BAB IV</a>
+    <a href="" class="btn-nature" style="transform: translatey(860px) translatex(1450px)">BAB V</a>
+</div>
+</body>
+</html>
