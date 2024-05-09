7ea6665993cfd8495c9005b9eb2b4751402107f2
Rein <musthafafaizhal@gmail.com>
2024-05-09 22:40:02 +0700

    Map 2
    broken wak

--- resources/views/maps.blade.php
+++ resources/views/maps.blade.php
@@ -1,30 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Poemventure</title>
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" >
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body class="container" style="
-  background-image: url('{{ url('img/map.jpg') }}');
-  background-size: cover;
-  background-repeat: no-repeat;
-  min-height: 100vh;
-">
-<div>
-    <a href="" class="btn-danger" style="transform: translatey(750px) translatex(125px)">BAB I</a>
-    <a href="" class="btn-dooms" style="transform: translatey(325px) translatex(465px)">BAB II</a>
-    <a href="" class="btn-sea" style="transform: translatey(700px) translatex(815px)">BAB III</a>
-    <a href="" class="btn-dwarf" style="transform: translatey(325px) translatex(1120px)">BAB IV</a>
-    <a href="" class="btn-nature" style="transform: translatey(860px) translatex(1450px)">BAB V</a>
-</div>
-</body>
-</html>
+<x-map-layout>
+    <a href="{{ route('kelasguru') }}" class="btn-danger" style="transform: translate();">BAB I</a>
+    <a href="{{ route('register') }}" class="btn-dooms" style="transform: translate();">BAB II</a>
+    <a href="{{ route('register') }}" class="btn-sea" style="transform: translate();">BAB III</a>
+    <a href="{{ route('register') }}" class="btn-dwarf" style="transform: translate();">BAB IV</a>
+    <a href="{{ route('register') }}" class="btn-nature" style="transform: translate();">BAB V</a>
+</x-map-layout>
