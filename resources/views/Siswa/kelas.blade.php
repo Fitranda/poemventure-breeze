fd13af8ef51ca06b6653d5c5d2aad39e4f0fa807
Fitranda Ramadhana <68302339+Fitranda@users.noreply.github.com>
2024-06-13 14:18:21 +0700

    mbenahi route

--- resources/views/Siswa/kelas.blade.php
+++ resources/views/Siswa/kelas.blade.php
@@ -10,11 +10,11 @@
 <div class="tes flex flex-wrap my-4">
     @foreach ($data as $item)
         <div class="mx-1 basis-1/3 my-2.5 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="{{route('map',['id'=>$item->id])}}">
+            <a href="{{route('prolog',['id'=>$item->id])}}">
                 <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{{$item->Nama}}</h5>
             </a>
             <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">{{$item->Deskripsi}}</p>
-            <a href="{{route('map',['id'=>$item->id])}}" class="inline-flex font-medium items-center text-blue-600 hover:underline">
+            <a href="{{route('prolog',['id'=>$item->id])}}" class="inline-flex font-medium items-center text-blue-600 hover:underline">
                 {{$item->kode_kelas}}
                 <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
--- resources/views/dashboardsiswa.blade.php
+++ resources/views/dashboardsiswa.blade.php
@@ -1,4 +1,4 @@
 <x-app-layout>
     <span class="centered-text">Selamat Datang {{ Auth::user()->name }}</span>
-    <a href="{{ route('prolog',['id'=>1]) }}" class="btn-custom" style="transform: translate(-70%, 100%);">Kelas</a>
+    <a href="{{ route('kelassiswa') }}" class="btn-custom" style="transform: translate(-70%, 100%);">Kelas</a>
 </x-app-layout>
